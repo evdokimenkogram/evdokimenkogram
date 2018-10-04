2d4fb7a619dba4289d10759f198baede258d8730
evdokimenkogram <39723856+evdokimenkogram@users.noreply.github.com>
2018-10-04 22:50:52 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+
+ 
+
+function g()
+
+{
+
+if(document.getElementById("sl1").selectedIndex==0)
+document.getElementById("pic1").src="2.jpg";
+
+
+
+if(document.getElementById("sl1").selectedIndex==1)
+document.getElementById("pic1").src="3.jpeg";
+
+
+
+ 
+
+if(document.getElementById("sl1").selectedIndex==2)
+document.getElementById("pic1").src="4.jpg";
+}
+
+function f1() 
+{
+  alert( 'Привет !' );
+}
