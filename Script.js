95b881d77ca98ae17be92eb12cdcbe289fb2c1be
evdokimenkogram <39723856+evdokimenkogram@users.noreply.github.com>
2018-10-04 23:28:45 +0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,16 +1,16 @@
 ﻿function f4()
 {
-document.getElementById("T5").src="03.png";
+document.getElementById("T5").src="04.jpg";
 }
 function f3()
 {
-document.getElementById("T5").src="04.jpg";
+document.getElementById("T5").src="03.png";
 }
 function f2()
 {
-document.getElementById("T5").src="02.jpg";
+document.getElementById("T5").src="01.gif";
 }
 function f1()
 {
-document.getElementById("T5").src="01.gif";
+document.getElementById("T5").src="02.jpg";
 }
