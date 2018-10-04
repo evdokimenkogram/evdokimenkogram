004bbb2de5f92e74a4b418f7ec7e31f59a919d66
evdokimenkogram <39723856+evdokimenkogram@users.noreply.github.com>
2018-10-04 23:13:52 +0300

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,16 @@
+﻿function f4()
+{
+document.getElementById("T5").src="03.png";
+}
+function f3()
+{
+document.getElementById("T5").src="04.jpg";
+}
+function f2()
+{
+document.getElementById("T5").src="02.jpg";
+}
+function f1()
+{
+document.getElementById("T5").src="01.gif";
+}
